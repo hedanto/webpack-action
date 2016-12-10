e46f0a0392f6b1974a105c3072cff40f95a34dd1
tim <642752012@qq.com>
2016-12-10 16:46:10 +0800

    multiple entry points

---
+++ demo-02/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: {
+    bundle1: './main1.js'/*,
+    bundle2: './main2.js'*/
+  },
+  output: {
+    filename: '[name].js',
+    chunkFilename: "[name].min.js"
+  }
+};
