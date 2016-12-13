dba305bc0984fcb1dd8446b23541c343bc4d4d1f
tim <642752012@qq.com>
2016-12-13 11:15:29 +0800

    webpack-dev-serverl demo

--- demo-06/src/main.js
+++ demo-06/src/main.js
@@ -1,5 +1,4 @@
 document.write('<h1>Hello World</h1>');
-
 var img2 = document.createElement("img");
 img2.src = require("./img/big.png");
 document.body.appendChild(img2);
--- demo-06/webpack.config.js
+++ demo-06/webpack.config.js
@@ -1,30 +1,22 @@
 var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist/'),
-        publicPath:'../dist/',
-        pathinfo:true,
-        sourceMapFilename:'[file].map',
-        chunkFilename: '[id].[chunkhash].js',
-        devtoolModuleFilenameTemplate: info => {
-            return `webpack:///${info.resourcePath}?${info.loaders}`;
-        }
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
     },
     module: {
-        loaders:[
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+        loaders: [
+            {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
         ]
     },
     devtool: 'cheap-module-source-map',
     devServer: {
-        /*contentBase: path.join(__dirname, "dist"),*/
-        compress: true,
-        port: 9000,
-        clientLogLevel:'none',
-        hot:true
+        historyApiFallback: true,
+        noInfo: true,
+        port:9000
     }
-
 };
